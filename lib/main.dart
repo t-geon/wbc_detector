ba90a802afe39d779721461d562934b1af2eaa26
t-geon <geon2331@naver.com>
2022-03-07 12:00:05 +0900

    patient list (access db)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-/*
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:wbc_detector/route.dart';
@@ -21,12 +20,12 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-*/
 
+/*
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:wbc_detector/pages/example_page.dart';
+import 'pages/list/list_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,34 +33,18 @@ void main() async {
   runApp(MyApp());
 }
 
-/*void main() {
-  runApp(MyApp());
-}*/
-
-Future testData() async {
-  await Firebase.initializeApp();
-  FirebaseFirestore db = FirebaseFirestore.instance;
-
-  var data = await db
-      .collection('patient_details')
-      .get(); //patient_details컬렉션에서 get메서드이용해 모든 데이터 가져옴
-  var details = data.docs.toList(); //null이 아닌경우 details리스트에 넣는다.
-
-  details.forEach((d) {
-    print(d.id); //id는 firestore내부 고유 식별자
-  });
-}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    testData();
     return MaterialApp(
       title: 'PatientDetail',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ), //테마 설정
-      home: PatientScreen(),
+      //theme: ThemeData(
+      // primarySwatch: Colors.orange,
+      //), //테마 설정
+      home: ListPage(),
     );
   }
 }
+
+ */
--- lib/pages/list/list_page.dart
+++ lib/pages/list/list_page.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:wbc_detector/components/patient_card.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:wbc_detector/model/patient.dart';
 
-//검사목록 페이지
 class ListPage extends StatelessWidget {
   static String routeName = "/list";
-  final List<String> list = List.generate(10, (index) => "Text $index");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: PatientList());
+  }
+}
+
+class PatientList extends StatefulWidget {
+  PatientList() {
+    Firebase.initializeApp();
+  }
+
+  @override
+  _PatientListState createState() => _PatientListState();
+}
+
+class _PatientListState extends State<PatientList> {
+  final FirebaseFirestore db = FirebaseFirestore.instance;
+  List<PatientDetail> details = []; //firestore의 인스턴스  저장될 list
 
   @override
   Widget build(BuildContext context) {
@@ -17,10 +36,43 @@ class ListPage extends StatelessWidget {
       body: ListView(
         //리스트 생성
         children: List.generate(
-          patients.length,
-          (index) => PatientCard(patient: patients[index]),
+          (details != null) ? details.length : 0,
+          (position) => PatientCard(details: details[position]),
+          //patientcard에 details전달해서 검사목록생성
         ),
       ),
     );
+  }
+
+  void initState() {
+    //화면이 로드 되면 getDetailList 호출하고 시간 지나면 정보 업데이트 된다.
+    if (mounted) {
+      getDetailList().then((data) {
+        setState(() {
+          details = data;
+        });
+      });
+    }
+    super.initState();
+  }
+
+  //getDetailList는 State가 변경될 때마다 호출되기 때문에 build안에서 호출하면 안됨 -> 무한루프 문제생김
+  //getDetailList는 위젯 생성될 때 호출
+  Future<List<PatientDetail>> getDetailList() async {
+    //cloud firestore의 patient_details컬랙션에서 가져온다
+    var data = await db.collection('patient_details').get();
+
+    if (data != null) {
+      //data가 null이 아닐 때 id를 문서의 id인스턴스로 설정
+      details =
+          data.docs.map((document) => PatientDetail.fromMap(document)).toList();
+
+      int i = 0;
+      details.forEach((detail) {
+        detail.id = data.docs[i].id;
+        i++;
+      });
+    }
+    return details;
   }
 }
